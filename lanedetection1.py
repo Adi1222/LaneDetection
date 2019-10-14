7be24ae4801a1d41a68e244c9217b4758c13e844
Adi1222 <53761308+Adi1222@users.noreply.github.com>
2019-10-14 15:10:31 +0530

    Update lanedetection1.py
    removed a line

--- lanedetection1.py
+++ lanedetection1.py
@@ -14,7 +14,7 @@ def region_of_interest(img, vertices):
 
 def draw_the_lines(img, lines):
     img = np.copy(img)
-    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype = np.uint8)
+   
     for line in lines:
         for x1,y1,x2,y2 in line:
             cv2.line(blank_image, (x1,y1),(x2,y2), (0,255,0), thickness = 4)
